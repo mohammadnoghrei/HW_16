a4b572ec31d7be84938402d1dbfed307bace511c
mohammadnoghrei <noghreimohammad@gmail.com>
2024-04-23 01:36:16 +0330

    create SecurityContext class

---
+++ src/main/java/madsilver/SecurityContext.java
@@ -0,0 +1,19 @@
+package madsilver;
+
+import madsilver.model.ProfessorType;
+
+public class SecurityContext {
+
+    public static long id;
+
+    public static String personalCode;
+    public static String password;
+    public static String nationalCode;
+    public static String firstName;
+    public static String lastName;
+    public static String major;
+    public static String description;
+    public static ProfessorType professorType;
+
+
+}
